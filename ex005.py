5ddcc5be790411f1d81fde25efa335bd41a63e25
João Felipe Carlos Rodrigues <joaofelipecrodrigues@gmail.com>
2022-07-03 21:26:43 -0300

    :truck: mv ex003-ex008

--- ex005.py
+++
@@ -1,2 +0,0 @@
-num = int(input('Digite um número: '))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(num,(num-1),(num+1)))
--- ex007.py
+++
@@ -1,5 +0,0 @@
-n1 = float(input('Primeira nota do aluno: '))
-n2 = float(input('Segunda nota do aluno: '))
-med = (n1+n2)/2
-
-print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1,n2,med))
