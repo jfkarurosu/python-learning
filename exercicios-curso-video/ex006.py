94073e0bb07ad3798cfa64f3a55978675eb9df15
João Felipe Carlos Rodrigues <joaofelipecrodrigues@gmail.com>
2022-07-03 21:29:10 -0300

    :truck: mv exerc

---
+++ exercicios-curso-video/ex006.py
@@ -0,0 +1,4 @@
+num = int(input('Digite um valor: '))
+print('O dobro de {} vale {}.'.format(num,(num*2)))
+print('O triplo de {} vale {}.'.format(num,(num*3)))
+print('A raiz quadrada de {} é igual a {:.2f}.'.format(num,(num**0.5)))
